cd94ea98771c1a6c923888c53d3f897427ccceae
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2023-01-12 20:20:00 -0300

    add/spanish

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -72,7 +72,7 @@ export const NavBar = () => {
             <li className={Styles.nav_menu_item}>
               <div className={Styles.nav_leguaje}>
                 <BiWorld />
-                <a href="#contact">English</a>
+                <a href="https://portfolio-en-psi.vercel.app/">English</a>
               </div>
             </li>
           </ul>
