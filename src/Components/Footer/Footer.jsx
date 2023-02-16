f7ce786438e0f21fe22861d027fbf0cc22edec6d
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2023-02-16 13:15:33 -0300

    fix styles

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,36 +1,65 @@
-import React from 'react';
+import React from "react";
 import Styles from "./Footer.module.css";
-import { AiOutlineInstagram, AiOutlineLinkedin, AiOutlineTwitter } from "react-icons/ai";
+import {
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+  AiOutlineTwitter,
+} from "react-icons/ai";
 
 export const Footer = () => {
   return (
-      <footer className={Styles.footer}>
-          <div className={Styles.footer_container}>
-            <h1 className={Styles.footer_title}>Luis Goytia</h1>
+    <footer className={Styles.footer}>
+      <div className={Styles.footer_container}>
+        <h1 className={Styles.footer_title}>Luis Goytia</h1>
 
-            <ul className={Styles.footer_list}>
-                <li>
-                    <a href="#about" className={Styles.footer_link}>About</a>
-                </li>
-                <li>
-                    <a href="#skills" className={Styles.footer_link}>Skills</a>
-                </li>
-                <li>
-                    <a href="#portfolio" className={Styles.footer_link}>Proyects</a>
-                </li>
-            </ul>
+        <ul className={Styles.footer_list}>
+          <li>
+            <a href="#about" className={Styles.footer_link}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className={Styles.footer_link}>
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio" className={Styles.footer_link}>
+              Proyects
+            </a>
+          </li>
+        </ul>
 
-            <div className={Styles.footer_social}>
-            <a href="https://twitter.com/LuisGoytiaDev" className={Styles.footer_social_icon} target="_blank"><AiOutlineTwitter /></a>
+        <div className={Styles.footer_social}>
+          <a
+            href="https://twitter.com/LuisGoytiaDev"
+            className={Styles.footer_social_icon}
+            target="_blank"
+          >
+            <AiOutlineTwitter />
+          </a>
 
-            <a href="https://www.instagram.com/luisgoytia.dev/" className={Styles.footer_social_icon} target="_blank"><AiOutlineInstagram /></a> 
+          <a
+            href="https://www.instagram.com/luisgoytia.dev/"
+            className={Styles.footer_social_icon}
+            target="_blank"
+          >
+            <AiOutlineInstagram />
+          </a>
 
-            <a href="https://www.linkedin.com/in/luis-goytia/" className={Styles.footer_social_icon} target="_blank"><AiOutlineLinkedin /></a>  
-              </div>
-              <br />
-              <h5>Portfolio diseñado y desarrollado por Luis Fernando Goytia Cabezas</h5>
-          </div>
-          
-      </footer>
-  )
-}
+          <a
+            href="https://www.linkedin.com/in/luis-goytia/"
+            className={Styles.footer_social_icon}
+            target="_blank"
+          >
+            <AiOutlineLinkedin />
+          </a>
+        </div>
+        <br />
+        <h5>
+          Portfolio diseñado y desarrollado por Luis Goytia
+        </h5>
+      </div>
+    </footer>
+  );
+};
